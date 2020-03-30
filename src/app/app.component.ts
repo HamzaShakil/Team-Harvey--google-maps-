b2fc46311ff1f47a2d8a56e266bfe832d6ad7ef1
HamzaShakil <2hamzashakil96@gmail.com>
2020-03-30 12:50:01 +0500

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { OnChanges, AfterViewInit } from '@angular/core';
 import { GeocodesService } from './geocodes.service';
+import { MouseEvent, AgmMap } from '@agm/core';
 import { delay } from 'rxjs/operator/delay';
-//import {SlidingMarker} from '../SlidingMarker.min.js';
-//import {SlidingMarker} from 'marker-animate-unobtrusive';
 
-var markerStore = {};
 declare var google: any;
-
+var markerStore = {};
 var marker;
+var heatmap;
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
@@ -17,17 +16,15 @@ var marker;
 
 export class AppComponent implements OnChanges, AfterViewInit {
 
-
-  geoPoints: any[];
   zoom: number = 17;
-  markers: any[];
   map: any;
-
-  // initial center position for the map
-  lat: number = 24.880368
+  lat: number = 24.880368;
   lng: number = 67.045409;
+  getpoints:any = [];
+  // initial center position for the map
+
   onMapReady(map: any) {
-    console.log(map);
+ 
     this.map = map;
 
   }
@@ -43,12 +40,7 @@ export class AppComponent implements OnChanges, AfterViewInit {
 
 
     var self = this;
-    //self.myCoordinates.push({lat:"abc",lon:"no"});
-
-
     this.geo.getPositions().subscribe(geo => {
-      this.geoPoints = geo.points;
-      console.log(geo);
       geo.Points.forEach(function (res) {
         var marker = new google.maps.Marker({
           animation: google.maps.Animation.DROP,
@@ -56,17 +48,23 @@ export class AppComponent implements OnChanges, AfterViewInit {
           position: new google.maps.LatLng(res.lat, res.lon),
           title: res.lat.toString(),
           map: self.map,
-          cameraOnMarker: true,
+        
 
         });
 
-
+      self.getpoints.push(new google.maps.LatLng(res.lat, res.lon));
+      
       })
 
       this.drawpoints(geo);
-
+      console.log(this.getpoints);
 
     });
+   heatmap = new google.maps.visualization.HeatmapLayer({
+    data: this.getpoints(),
+    map: self.map
+  });
+
 
 
     setTimeout(function () {
@@ -276,34 +274,6 @@ export class AppComponent implements OnChanges, AfterViewInit {
         });
 
     }, 90000)
-    setTimeout(function () {
-      self.drawpoints(
-        {
-          "Points": [
-            {
-              "type": "person",
-              "id": 'A',
-              "lat": 24.883508,
-              "lon": 67.047160,
-              "data": {
-              }
-            },
-            {
-              "type": "person",
-              "id": 'B',
-              "lat": 24.882719,
-              "lon": 67.047589,
-              "data": {
-              }
-            }
-          ]
-
-        });
-
-    }, 10000)
-
-
-
 
   }
 
@@ -325,8 +295,7 @@ export class AppComponent implements OnChanges, AfterViewInit {
 
       if (markerStore.hasOwnProperty(res.id)) {
 
-        // self.lat = res.lat;
-        // self.lng = res.lon;
+       
        
         var myLatlng = new google.maps.LatLng(res.lat, res.lon);
        
@@ -357,9 +326,6 @@ export class AppComponent implements OnChanges, AfterViewInit {
           strokeWeight: 2
         });
         setInterval(function(){  flightPath.setMap(self.map);},1000);
-        //setTimeout(function(){  }, 1000);
-        //setTimeout()
-        
         i++;
 
       }
@@ -374,17 +340,7 @@ export class AppComponent implements OnChanges, AfterViewInit {
           easing: "easeOutQuad"
 
         });
-        // var marker = new google.maps.Marker({
-        //   // animation: google.maps.Animation.DROP,
-
-        //   position: latlng,//new google.maps.LatLng(res.lat, res.lon),
-        //   title: res.lat.toString(),
-        //   map: self.map,
-        //   cameraOnMarker: true,
-        //   duration: 2000,
-        //   easing: "easeOutExpo"
-
-        // });
+      
        
         marker.setPosition(latlng);
         markerStore[res.id] = marker;
